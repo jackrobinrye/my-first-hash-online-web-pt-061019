f891cb75ce502a3f16464c884af5cfca82362031
Jack Atherton <jackrrye@gmail.com>
2019-06-27 23:19:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {
     whale_bone_corsets: 5, 
-    :porcelain_vases => 2, 
+    porcelain_vases 2, 
     oil_paintings: 3
     
   }
